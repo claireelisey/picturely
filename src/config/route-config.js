5f602ba29e79c202f8dc1b3073515784c0211558
Claire Ouellette <claireelisey@gmail.com>
2018-12-15 11:34:06 -0600

    implemented user resource

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,13 +1,17 @@
 module.exports = {
 
     init(app){
+
         const staticRoutes = require("../routes/static");
         const albumRoutes = require("../routes/albums");
         const postRoutes = require("../routes/posts");
+        const userRoutes = require("../routes/users");
 
         app.use(staticRoutes);
         app.use(albumRoutes);
         app.use(postRoutes);
+        app.use(userRoutes);
+        
     }
     
 }
